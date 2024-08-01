e10714c11c959d8189ff8d6077e8315b58c8201b
Ayush Gupta <guptaayush6122@gmail.com>
2024-08-01 23:28:04 +0530

    commit

--- EMS4.0/src/main/java/com/example/demo/service/servicelayer.java
+++ EMS4.0/src/main/java/com/example/demo/service/servicelayer.java
@@ -1944,7 +1944,7 @@ String designarionArrowSplit = user.getDesignation();
             
             // Convert formattedDate back to Date object
             Date loginDate = parseDateInIST(formattedDate);
-            System.out.println("INDIA ASIA/KOLKATA "+loginDate);
+            System.out.println("----------------------------------------INDIA ASIA/KOLKATA-------------------------------- "+loginDate);
 			userLoginDateTime.setLoginDateAndTime(loginDate);
 			userLoginDateTime.setUsername(user.getUsername());
 			userLoginDao.save(userLoginDateTime);
@@ -1977,7 +1977,7 @@ String designarionArrowSplit = user.getDesignation();
 	            
 	            // Convert formattedDate back to Date object
 	            Date loginDate = parseDateInIST(formattedDate);
-	            System.out.println("INDIA ASIA/KOLKATA "+loginDate);
+	            System.out.println("-------------------------INDIA ASIA/KOLKATA------------------- "+loginDate);
 				userLoginDateTime.setLoginDateAndTime(loginDate);
 				userLoginDateTime.setUsername(user.getUsername());
 				userLoginDao.save(userLoginDateTime);
